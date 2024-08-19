153c480e4c27cd1ef6c2930c0a85a278b671ea86
DanielRojas <141978942+DanielBazanRojas@users.noreply.github.com>
2024-08-18 19:01:15 -0500

    Update pruebaIA.py

--- pruebaIA.py
+++ pruebaIA.py
@@ -29,7 +29,7 @@ while True:
 		cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
 		
 		#FisherFace
-		if result[1] < 500:
+		if result[1] < 280:
 			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
 			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
 		else:
